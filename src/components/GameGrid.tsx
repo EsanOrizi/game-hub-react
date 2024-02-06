9777d73c566f3e0aaa99c9aa6711bb074f7dbb86
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2024-02-06 00:06:54 +0000

    Displaying the Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const skeletons = [1, 2, 3, 4, 5, 6];
     return (
         <>
             {error && <Text>{error}</Text>}
-       <SimpleGrid columns={{sm:1 , md: 2, lg: 3, xl:4}} spacing={10} padding={10}>
+       <SimpleGrid columns={{sm:1 , md: 2, lg: 3, xl:4}} spacing={3} padding={10}>
            {isLoading && skeletons.map((skeleton) => <GameCardContainer><GameCardSkeleton  key={skeleton}/> </GameCardContainer> )}
            {data.map(data => <GameCardContainer><GameCard key={data.id} game={data} /> </GameCardContainer> )}
        </SimpleGrid>
